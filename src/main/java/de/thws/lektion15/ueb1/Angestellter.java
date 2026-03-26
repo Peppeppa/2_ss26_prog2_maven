7dbb9eb9d4f8feedea87eab991ec2e6069f61097
Stephen Eyring <accounts@eyring-s.de>
2026-03-26 09:00:15 +0100

    L14 4

---
+++ src/main/java/de/thws/lektion15/ueb1/Angestellter.java
@@ -0,0 +1,12 @@
+package de.thws.lektion15.ueb1;
+
+public class Angestellter {
+    String nachname;
+    String vorname;
+    String identifikator;
+    double grundgehalt;
+    int gehaltsfaktor;
+    String geburtsdatum;
+
+    public
+}
