06be792435300c5179e601dbdbc7b694499f01a3
Stephen Eyring <accounts@eyring-s.de>
2026-04-17 14:21:45 +0200

    L16 fertig

---
+++ src/main/java/de/thws/lektion17/exception/Exceptions.java
@@ -0,0 +1,19 @@
+package de.thws.lektion17.exception;
+import java.util.Scanner;
+
+public class Exceptions {
+    public static void main(String[] args){
+        // a
+        //double d = Double.parseDouble("abc");
+
+        //b
+//        int[] arr = new int[8];
+//        for (int i=0; i <= 9; i++){
+//            arr[i] = 2;
+//        }
+
+        //c
+
+
+    }
+}
