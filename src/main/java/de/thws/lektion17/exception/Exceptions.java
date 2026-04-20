2a4476f330410919b669296ff9d1f17d7376e38b
Stephen Eyring <accounts@eyring-s.de>
2026-04-20 10:53:10 +0200

    L17

--- src/main/java/de/thws/lektion17/exception/Exceptions.java
+++ src/main/java/de/thws/lektion17/exception/Exceptions.java
@@ -1,7 +1,9 @@
-package de.thws.lektion17.exception;
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Exceptions {
+
+}
     public static void main(String[] args){
         // a
         //double d = Double.parseDouble("abc");
@@ -13,7 +15,30 @@ public class Exceptions {
 //        }
 
         //c
+//        Object o = null;
+//        o.toString();
 
 
-    }
+        // stackoverflow
+//        leak();
+//    }
+//
+//    static void leak() {
+//        byte[] data = new byte[1024 * 1024]; // 1 MB
+//
+//        // Referenz wird über den Stack gehalten → nicht GC-fähig
+//        leak();
+//
+//    }
+        //d
+
+//
+//        public static void main(String[] args) {
+//            fill();
+//        }
+//
+//        static void fill() {
+//            heap.add(new byte[5 * 1024 * 1024]); // 5 MB
+//            fill();
+//        }
 }
